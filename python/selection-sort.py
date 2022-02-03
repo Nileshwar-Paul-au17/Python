e0a10de83fcc7f0e903fe407f35c839e854f8d89
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-02-03 13:39:38 +0530

    selection sort modify

--- python/selection-sort.py
+++ python/selection-sort.py
@@ -3,20 +3,19 @@ def selection_sort(A):
 
     n = len(A)
     for i in range(n):
-        
-      
-        for j in range(i,len(A)):
-            if A[j] < A[i]:
-                 
-                
-                A[i],A[j] = A[j],A[i]
-                 
-        print(A)
+        min=i
+        for j in range(i+1,n):
+            if A[j] < A[min]:
+                min=j
+        if(min!=i):
+            A[i],A[min]=A[min],A[i]
+        #print(A)
+        #print(" ")
     return A
 
 if __name__=="__main__":
     
-     #A = [64,25,12,22,11]
-     A = [2,0,1]
+    A = [64,25,12,22,11,0,-1,33]
+    #A=[2,0,1]
 
-     print(selection_sort(A)) 
+    print("After Sorting:",selection_sort(A)) 
