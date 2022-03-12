6d402ebcf22759bdd4d4ed23a7f2028c437fc056
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-03-12 20:45:21 +0530

    pushed

--- M5/BinarySubArray.py
+++ M5/BinarySubArray.py
@@ -44,7 +44,7 @@ def longestSubSeg(a, n, k):
                 cnt0 -= 1
             l += 1
         
-        max_len = max(max_len, i - l + 1);
+        max_len = max(max_len, i - l + 1)
    
     return max_len
 
