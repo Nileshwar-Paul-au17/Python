d330755da371864cc7341be8fe2b80e3c2ac3331
Nileshwar-Paul-au17 <776paul@gmail.com>
2022-04-23 11:53:49 +0530

    arrayPartition

---
+++ FPRT Bhabha apr/arrayPartition.py
@@ -0,0 +1,39 @@
+'''
+Given an array A of size N consisting of N integers.
+Print "YES" if you can partition the array into two non-empty subarray such that every number in the left subarray is strictly less than every number in the right subarray.
+
+Input Format
+
+First line of input contains integer N (size of the array)
+Second line of input contains N space seperated integers (A[0] , A[1] , A[2] ... A[N-1])
+Constraints
+
+1 <= N <=100000
+1 <= A[i] <=100000
+Output Format
+
+Print "YES" or "NO"
+
+Sample Input 0
+
+4
+5 3 2 7 9
+Sample Output 0
+
+YES
+Explanation 0
+
+We can split the list into left = [5, 3, 2] and right = [7, 9] . Every element in left subarray is strictly less than every element in right subarray
+
+Contest ends in an hour
+Submissions: 21
+Max Score: 25
+Difficulty: Medium
+Rate This Challenge:
+
+    
+More
+ 
+1
+​
+'''
