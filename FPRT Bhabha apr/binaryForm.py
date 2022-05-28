49dcc589793549e4c465c854569cdab98107a7e1
Nileshwar-Paul-au26 <776paul@gmail.com>
2022-05-28 23:38:49 +0530

    pushed

--- FPRT Bhabha apr/binaryForm.py
+++ FPRT Bhabha apr/binaryForm.py
@@ -27,62 +27,52 @@ Explanation 0
 Both 5 and 6 have same number of 0s and 1s
 '''
 from collections import Counter
-  
-def check(num1,num2):
-  
+
+
+def check(num1, num2):
+
     bin1 = bin(num1)[2:]
     bin2 = bin(num2)[2:]
-  
-   
+
+    print(bin1, bin2)
     zeros = abs(len(bin1)-len(bin2))
-    if (len(bin1)>len(bin2)):
-         bin2 = zeros * '0' + bin2
+    if (len(bin1) > len(bin2)):
+        bin2 = zeros * '0' + bin2
     else:
-         bin1 = zeros * '0' + bin1
-  
-   
+        bin1 = zeros * '0' + bin1
+
+    print(bin1, bin2)
     dict1 = Counter(bin1)
     dict2 = Counter(bin2)
-  
-  
+
     if dict1 == dict2:
-         print('YES')
+        print('YES')
     else:
-         print('NO')
- 
-num1,num2=list(map(int,input().split()))[:2]
+        print('NO')
+
+
+num1, num2 = list(map(int, input().split()))[:2]
 
 check(num1,num2)
 #################################################
-from collections import Counter
-  
-def checkAnagram(num1,num2):
-  
-    # convert numbers into in binary
-    # and remove first two characters of 
-    # output string because bin function 
-    # '0b' as prefix in output string
-    bin1 = bin(num1)[2:]
-    bin2 = bin(num2)[2:]
-  
-    # append zeros in shorter string
-    zeros = abs(len(bin1)-len(bin2))
-    if (len(bin1)>len(bin2)):
-         bin2 = zeros * '0' + bin2
-    else:
-         bin1 = zeros * '0' + bin1
-  
-    # convert binary representations 
-    # into dictionary
-    dict1 = Counter(bin1)
-    dict2 = Counter(bin2)
-  
-    # compare both dictionaries
-    if dict1 == dict2:
-         print('YES')
-    else:
-         print('NO')
- 
-num1,num2=list(map(int,input().split()))[:2]
 
-checkAnagram(num1,num2)
+# L,R=list(map(int,input().split()))[:2]
+# L=bin(L).replace("0b", "")
+# R=bin(R).replace("0b", "")
+# print(L,R)
+# a,b,c,d=0,0,0,0
+# for i in range(0,len(L)):
+#     if L[i]=='0':
+#         a+=1
+#     else:
+#         b+=1
+# for i in range(0,len(R)):
+#     if R[i]=='1':
+#         c+=1
+#     else:
+#         d+=1
+
+# if(a==d and b==c):
+#     print("YES")
+# else:
+#     print("NO")
